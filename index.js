af086cbaf1eba74fdfc0b75c729363a611ae30f4
Alex-Reyne <creativereyne@gmail.com>
2021-10-29 21:02:14 -0600

    fix a couple of lint issues

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
 function sum(fromN, toN) {
 
   if (fromN === toN) {
     return fromN;
   }
 
-  return fromN + sum(fromN + 1, toN);;
+  return fromN + sum(fromN + 1, toN);
 };
 
 module.exports = sum;
